1fda7c6cb6a84773e7e5c04b5d1c4f184b8a9505
TheZxSmallboy <zixuan_wang@mymail.sutd.edu.sg>
2022-06-10 10:00:15 +0800

    update

--- offboardcontrol.py
+++ offboardcontrol.py
@@ -37,21 +37,21 @@ async def run():
         await drone.action.disarm()
         return
 
-    print("-- Go 0m North, 0m East, -5m Down within local coordinate system")
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, -5.0, 0.0))
-    await asyncio.sleep(10)
+    print("-- Go 0m North, 0m East, -20m Down within local coordinate system")
+    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, -20.0, 0.0))
+    await asyncio.sleep(15)
 
     print("-- Go 5m North, 0m East, -5m Down within local coordinate system, turn to face East")
-    await drone.offboard.set_position_ned(PositionNedYaw(5.0, 0.0, -5.0, 90.0))
-    await asyncio.sleep(10)
+    await drone.offboard.set_position_ned(PositionNedYaw(50.0, 0.0, -20.0, 0.0))
+    await asyncio.sleep(15)
 
     print("-- Go 5m North, 10m East, -5m Down within local coordinate system")
-    await drone.offboard.set_position_ned(PositionNedYaw(5.0, 10.0, -5.0, 90.0))
+    await drone.offboard.set_position_ned(PositionNedYaw(100.0, 0.0, -20.0, 0.0))
     await asyncio.sleep(15)
 
     print("-- Go 0m North, 10m East, 0m Down within local coordinate system, turn to face South")
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 10.0, 0.0, 180.0))
-    await asyncio.sleep(10)
+    await drone.offboard.set_position_ned(PositionNedYaw(300.0, 0.0, -20.0, 0.0))
+    await asyncio.sleep(15)
 
     print("-- Stopping offboard")
     try:
